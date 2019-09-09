6e34f68be8d63fcd40367ad58f433561168cc2e2
DrakSkoldpadda <gabriel.gonzalezleonardsson@elev.ga.lbs.se>
2019-09-09 18:54:47 +0200

    gam

--- 2drpg/Assets/Scripts/Player.cs
+++ 2drpg/Assets/Scripts/Player.cs
@@ -5,35 +5,71 @@ using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Tilemap obstaclesTilemap;
-    [SerializeField] public Tilemap groundTilemap;
-
-    [SerializeField] public float moveTime;
+    [SerializeField] private float moveSpeed = 3f;
     private bool isMoving = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    private Vector2 input;
+    private Vector3 startPos;
+    private Vector3 endPos;
 
-    }
+    private float time;
+
+    [SerializeField] private Collider2D leftCollider;
+    [SerializeField] private Collider2D topCollider;
+    [SerializeField] private Collider2D rightCollider;
+    [SerializeField] private Collider2D bottomCollider;
 
     // Update is called once per frame
     void Update()
     {
-
+        Movement();
     }
 
     private void Movement()
     {
-        float Xinput = Input.GetAxisRaw("Horizontal");
-        float Yinput = Input.GetAxisRaw("Vertical");
-
-        if (Xinput != 0f)
+        if (!isMoving)
         {
-            Yinput = 0f;
+            input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+            CheckMove();
+
+            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+            {
+                input.y = 0f;
+            }
+            else
+            {
+                input.x = 0f;
+            }
+
+            if (input != Vector2.zero)
+            {
+                StartCoroutine(Move(transform));
+            }
+        }
+    }
+
+    private void CheckMove()
+    {
+
+    }
+
+    private IEnumerator Move(Transform entity)
+    {
+        isMoving = true;
+        startPos = entity.position;
+        time = 0;
+
+        endPos = new Vector3(startPos.x + System.Math.Sign(input.x), startPos.y + System.Math.Sign(input.y), startPos.z);
+
+        while (time < 1f)
+        {
+            time += Time.deltaTime * moveSpeed;
+            entity.position = Vector3.Lerp(startPos, endPos, time);
+            yield return null;
         }
 
-
-        //Vector3 newPosition = new Vector3.MoveTowards()
+        isMoving = false;
+        yield return 0;
     }
 }
