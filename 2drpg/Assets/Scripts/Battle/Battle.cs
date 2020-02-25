d3b8763160fc2d33818e73fe8c2de30d2977aa93
DrakSkoldpadda <42768824+DrakSkoldpadda@users.noreply.github.com>
2020-02-25 12:26:45 +0100

    yes

---
+++ 2drpg/Assets/Scripts/Battle/Battle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battle : MonoBehaviour
+{
+    [SerializeField] private int damage = 5;
+    [SerializeField] private float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Attack(Transform target)
+    {
+        float step = speed * Time.deltaTime;
+
+        Vector3.MoveTowards(transform.position, target.position, step);
+
+        //target.GetComponent<Health>().TakeDamage(damage);
+    }
+}
